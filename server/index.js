6f642b4e44384da6351b3225b0c19587459d52e9
Abhishek Agarwal <aasuper1@gmail.com>
2020-04-04 23:39:52 -0400

    initial mongo schema model def

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const simulator = require("./generateSimulation");
 
+// Loads up all models in models directory
+//https://www.youtube.com/watch?v=5e1NEdfs4is (FOLLOW THIS TUTORIAL)
+var fs = require('fs');
+fs.readdirSync(__dirname + '/models').forEach(function(filename) {
+    if (~filename.indexOf('.js')) require(__dirname + '/models/' + filename)
+});
+
+
 let app = express();
 app.use(cors());
 const bodyParser = require('body-parser');
@@ -11,7 +19,9 @@ app.get('/', (req, res) => {
     res.send("OHQ Simulator");
 });
 
-let data = null;
+let data = null; // This variable needs to be converted into a database stored locally
+// Add a field to do simulation request
+// 
 
 app.post('/doSimulation', (req,res) => {
     data = simulator.simulate(req.body.numTas, req.body.mean_arrival_time, req.body.alphas, req.body.lambdas);
@@ -28,4 +38,5 @@ app.get('/getSimulationData', (req, res) => {
 
 app.listen(3001, () => {
     console.log('server listening at port 3001');
+    // View using http://localhost:3001/
 });
